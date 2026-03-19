6f35549d3faec399617b293469e88a48ace7145d
FelicityMnguni <131159388+FelicityMnguni@users.noreply.github.com>
2026-03-19 15:36:25 +0200

    Update extract.py

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,6 +1,21 @@
 import pandas as pd
 
-def extract_data(file):
-    """Extract data from uploaded CSV"""
-    df = pd.read_csv(file)
-    return df
+def extract_all():
+
+    bi = pd.read_csv("data/bi_logging.csv", 
+                     parse_dates=["Date"],
+                     dayfirst=True)
+    bi["Horizon"] = bi["Horizon"].astype(str).str.strip().str.upper()
+    
+    risk = pd.read_csv("data/internal_risks.csv")
+    sdg = pd.read_csv("data/SDGs.csv")
+    wef_short = pd.read_csv("data/WEF Risks- short term.csv")
+    wef_long = pd.read_csv("data/WEF Risks- long term.csv")
+
+    return {
+        "bi": bi,
+        "risk": risk,
+        "sdg": sdg,
+        "wef_short": wef_short,
+        "wef_long": wef_long
+    }
