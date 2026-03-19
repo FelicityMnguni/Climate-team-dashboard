dbf2ae6e1aaee2c8717e4e480cc9bd88e8035515
FelicityMnguni <131159388+FelicityMnguni@users.noreply.github.com>
2026-03-19 15:43:14 +0200

    Update extract.py

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
-def extract_all():
 
-    bi = pd.read_csv("data/bi_logging.csv", 
-                     parse_dates=["Date"],
-                     dayfirst=True)
+def extract_all(bi_file, risk_file, sdg_file=None, wef_short_file=None, wef_long_file=None):
+    bi = pd.read_csv(bi_file, parse_dates=["Date"], dayfirst=True)
     bi["Horizon"] = bi["Horizon"].astype(str).str.strip().str.upper()
-    
-    risk = pd.read_csv("data/internal_risks.csv")
-    sdg = pd.read_csv("data/SDGs.csv")
-    wef_short = pd.read_csv("data/WEF Risks- short term.csv")
-    wef_long = pd.read_csv("data/WEF Risks- long term.csv")
+
+    risk = pd.read_csv(risk_file)
+    sdg = pd.read_csv(sdg_file) if sdg_file else pd.DataFrame()
+    wef_short = pd.read_csv(wef_short_file) if wef_short_file else pd.DataFrame()
+    wef_long = pd.read_csv(wef_long_file) if wef_long_file else pd.DataFrame()
 
     return {
         "bi": bi,
